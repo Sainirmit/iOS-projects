9e35719c13e10573e7797cf03dbcdc56b2a20f07
Sainirmit <sainirmit@gmail.com>
2024-04-07 13:34:22 +0530

    Add files via upload

---
+++ PersonalityQuiz/PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Sai Nirmit on 05/03/24.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func unwindToQuizIntroduction(segue:
+    UIStoryboardSegue) {
+        
+    }
+
+}
+
---
+++ PersonalityQuiz/PersonalityQuiz/ResultsViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ResultsViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Sai Nirmit on 05/03/24.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        calculatePersonalityResult()
+        
+        navigationItem.hidesBackButton = true
+    }
+    
+    func calculatePersonalityResult() {
+        let frequencyOfAnswers = responses.reduce(into:
+           [AnimalType: Int]()) { (counts, answer) in
+            if let existingCount = counts[answer.type] {
+                counts[answer.type] = existingCount + 1
+            } else {
+                counts[answer.type] = 1
+            }
+        }
+        _ = frequencyOfAnswers.sorted(by:
+        { (pair1, pair2) in
+            return pair1.value > pair2.value
+        })
+        
+        let mostCommonAnswer = frequencyOfAnswers.sorted { $0.1 >
+           $1.1 }.first!.key
+        
+        
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.defination
+        
+    }
+    
+
+    var responses: [Answer]
+
+    init?(coder: NSCoder, responses: [Answer]) {
+        self.responses = responses
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+  
+    
+    @IBOutlet var resultAnswerLabel: UILabel!
+    @IBOutlet var resultDefinitionLabel: UILabel!
+
+}
